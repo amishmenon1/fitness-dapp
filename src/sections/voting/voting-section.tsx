3d95a9ee65a56dad4aa634442a889dfc189fd05b
amishmenon1 <amishmenon@gmail.com>
2024-06-13 18:25:36 -0500

    responsive updates

--- src/sections/voting/voting-section.tsx
+++ src/sections/voting/voting-section.tsx
@@ -59,7 +59,7 @@ const VotingSection = () => {
 
   return (
     <section id="voting-card-container" className=" flex flex-col gap-8">
-      <div className=" h-50 flex justify-between ">
+      <div className=" flex sm:justify-between justify-center gap-4 xs:gap-10">
         <Card
           image={cardioGif}
           CardTitle="Cardio"
@@ -84,7 +84,7 @@ const VotingSection = () => {
       </div>
       <div
         id="score-container"
-        className=" rounded-lg bg-gray-600 shadow-lg duration-300 hover:shadow-3"
+        className=" rounded-lg bg-gray-600 shadow-lg duration-300 hover:shadow-3 xs:mx-0 mx-6 "
       >
         {isPending ? (
           <div>
@@ -94,10 +94,10 @@ const VotingSection = () => {
           </div>
         ) : (
           <div className="bg-gray-100 h-full shadow-lg rounded-lg">
-            <h2 className="xs:text-xl sm:text-3xl text-4xl text-gray-300 text-center bg-gradient-to-r from-black to-gray-400 font-bold py-4">
+            <h2 className="sm:text-3xl text-2xl text-gray-300 text-center bg-gradient-to-r from-black to-gray-400 font-bold py-4">
               Score
             </h2>
-            <div className="xs:text-xl sm:text-2xl text-3xl flex flex-col items-center justify-center p-8 space-y-4">
+            <div className="xs:text-xl sm:text-2xl text-lg flex flex-col items-center justify-center p-8 space-y-4">
               <div className="bg-white w-full py-4 rounded-md text-center shadow-md">
                 <h5 className=" text-gray-800 font-light">
                   Cardio: {cardioVotes?.result?.toString()}
