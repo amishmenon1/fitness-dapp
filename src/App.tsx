bc33f2d13e237e86fb7c6035d0c788a35d70b656
amishmenon1 <amishmenon@gmail.com>
2024-06-13 18:07:14 -0500

    ui updates

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="h-[100vh] bg-dumbbellBg bg-cover">
+    <div className="h-[100vh] bg-dumbbellBg bg-cover xs:px-10 sm:px-20 md:px-40">
       {/* className="h-[100vh] bg-gray-400 bg-cover" */}
 
       <NavBar />
--- src/pages/landing/landing-page.tsx
+++ src/pages/landing/landing-page.tsx
@@ -4,7 +4,7 @@ import VotingSection from "@/sections/voting/voting-section";
 
 export const LandingPage = () => {
   return (
-    <div className="h-full flex flex-col gap-4">
+    <div className="h-full flex flex-col gap-4 justify-center items-center">
       <VotingSection />
     </div>
   );
