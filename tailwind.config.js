c686f11c0754733f775ad5938d5d30f80e5c5d73
amishmenon1 <amishmenon@gmail.com>
2024-06-27 20:56:17 -0500

    bg image path

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,7 +21,7 @@ export default withMT({
       backgroundImage: () => ({
         "gradient-yellowred":
           "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
-        dumbbellBg: "url('./src/assets/bg-1.jpeg')",
+        dumbbellBg: "url('./bg-1.jpeg')",
       }),
       fontFamily: {
         dmsans: ["DM Sans", "sans-serif"],
