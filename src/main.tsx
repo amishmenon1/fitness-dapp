dfa50c9cf65c62a6d2e963473fb654cf51e1a425
amishmenon1 <amishmenon@gmail.com>
2024-08-09 16:54:47 -0500

    removed unused imports

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { createWeb3Modal } from "@web3modal/wagmi";
-import { wagmiConfig } from "./wagmi.config.ts";
-import { WagmiProvider } from "wagmi";
 import { BrowserRouter } from "react-router-dom";
 import Web3ModalProvider from "./contexts/web3modal-provider.tsx";
 
